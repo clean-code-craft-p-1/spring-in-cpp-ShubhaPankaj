b4975bb877cd8ac057f73cce02b05a00404324ce
ShubhaPankaj <shubha.pankaj@philips.com>
2023-07-21 21:02:19 +0530

    Resolving error regarding NAN

--- stats.h
+++ stats.h
@@ -3,8 +3,6 @@
 #include <cmath>
 #include <numeric>
 
-#define NAN (std::numeric_limits<double>::quiet_NaN())
-
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
     
